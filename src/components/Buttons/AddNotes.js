4c2660b481f83f9ed4022cd3723a4b7902e64b6e
Jordan Walker <jwalker117@gmail.com>
2020-04-30 08:59:18 -0500

    removed excess code on modal for AddNotes that was not being used

--- src/components/Buttons/AddNotes.js
+++ src/components/Buttons/AddNotes.js
@@ -1,16 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './modal.css';
 
 function AddNotes() {
   const outside = useRef()
   const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = (event) => {
-    if (outside.current.contains(event.target)) {
-      return
-    }
-    setIsOpen(false)
-  }
 
   return (
     <div ref={outside}>
@@ -20,7 +13,7 @@ function AddNotes() {
           <div className="modal_content">
             <h3>Notes:</h3>
             <article>
-                <textarea>lorem</textarea>
+                <textarea defaultValue="lorem"></textarea>
             </article>
             <button>Save</button>
             <button onClick={() => setIsOpen(!isOpen)}>Cancel</button>
