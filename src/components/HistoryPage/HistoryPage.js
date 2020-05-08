63b8fd8bd5ed6015a553e6a775f3ed7b0d275f89
Jordan Walker <jwalker117@gmail.com>
2020-05-08 11:48:33 -0500

    history page rendering table of finished speeches and delete button working

--- src/components/HistoryPage/HistoryPage.js
+++ src/components/HistoryPage/HistoryPage.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 //Import Individual Components to use on this page;
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
+import DeleteSpeech from '../Buttons/DeleteSpeech';
 
 class HistoryPage extends Component {
   componentDidMount() {
@@ -16,9 +17,27 @@ class HistoryPage extends Component {
     return (
       <div>
         <Nav />
-        <p>
-          Speech History Page
-        </p>
+        <h1>Speech History</h1>
+        <table className="history-table">
+          <thead>
+            <tr>
+              <th>Speech Name</th>
+              <th>Details</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.speeches.map((speech) => {
+              return(
+                <tr key={speech.id}>
+                  <td>{speech.speech_title}</td>
+                  <td><button>View Details</button></td>
+                  <td><DeleteSpeech speech_id={speech.id} user_id={speech.user_id}/></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
         <Footer />
       </div>
     );
@@ -28,7 +47,8 @@ class HistoryPage extends Component {
 
 //Set props to this component from redux store
 const mapStateToProps = reduxState => ({
-  user_id: reduxState.user.id
+  user_id: reduxState.user.id,
+  speeches: reduxState.finisheSpeech
 });
 
 export default connect(mapStateToProps)(HistoryPage);
