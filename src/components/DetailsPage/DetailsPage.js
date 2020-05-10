3a26bbc299e09b056bbecfddda9d3a871575b055
Jordan Walker <jwalker117@gmail.com>
2020-05-10 15:18:44 -0500

    Base mode completed and everything is working for presentation

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -9,11 +9,22 @@ import { withRouter } from 'react-router-dom';
 //Import Individual Components to use on this page;
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
+import Scroll from '../Scroll/Scroll2';
 
 class DetailsPage extends Component {
     componentDidMount() {
         this.getSpeechById();
     }
+
+  //Method to get seconds on DOM
+    getSeconds = () => {
+    return ('0' + this.props.speech.speech_rt % 60).slice(-2);
+  }
+
+  //Method to get minutes on the DOM
+    getMinutes = () => {
+    return Math.floor(this.props.speech.speech_rt / 60);  
+  }
 
     //Get the speech from the database and set to redux state
     getSpeechById = () => {
@@ -32,6 +43,42 @@ class DetailsPage extends Component {
       <div>
         <Nav />
         <h1>Details Page</h1>
+        <Scroll>
+            <section>
+                <h3>Speech Title</h3>
+                <h4>{this.props.speech.speech_title}</h4>
+                <h3>Speech Type</h3>
+                <h4>{this.props.speech.speech_type}</h4>
+            </section>
+            <section>
+                <h3>Speech Content</h3>
+                <p>{this.props.speech.speech_text}</p>
+            </section>
+            <section>
+                <h3>Speech Notes</h3>
+                <pre>{this.props.speech.notes}</pre>
+            </section>
+            <section>
+                <h3>Speech Table Topics</h3>
+                <pre>{this.props.speech.table_topics}</pre>
+            </section>
+            <section>
+                <h3>Speech Evaluation</h3>
+                <pre>{this.props.speech.speech_eval}</pre>
+            </section>
+            <section>
+                <h3>Speech Word Count</h3>
+                <h4>Ands: {this.props.speech.and_count}</h4>
+                <h4>Likes: {this.props.speech.like_count}</h4>
+            </section>
+            <section>
+              <h3>Time</h3>
+              <h4>Min:Sec = {this.getMinutes()}:{this.getSeconds()}</h4>
+              <h3>In Time?</h3>
+              {this.props.speech.in_time ? <h4>Yes</h4> : <h4>No</h4>}
+            </section>
+        </Scroll>
+        
         <Footer />
       </div>
     );
@@ -42,7 +89,7 @@ class DetailsPage extends Component {
 //Set props to this component from redux store
 const mapStateToProps = reduxState => ({
   user_id: reduxState.user,
-  speeches: reduxState.finisheSpeech
+  speech: reduxState.speech
 });
 
 export default withRouter(connect(mapStateToProps)(DetailsPage));
