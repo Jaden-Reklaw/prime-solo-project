1943b59a762832b34b6de0d3f4a984d585f9a281
Jordan Walker <jwalker117@gmail.com>
2020-05-12 20:11:20 -0500

    Added protection to all routes for put requests

--- server/routes/speech.router.js
+++ server/routes/speech.router.js
@@ -136,112 +136,132 @@ router.delete('/delete/:id', rejectUnauthenticated, (req, res) => {
  * PUT route uses UPDATE SQL
  * NOTES
  */
-router.put('/notes/:id', (req, res) => {
-    let notes = req.body.notes;
-    let speech_id = req.params.id;
+router.put('/notes/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        let notes = req.body.notes;
+        let speech_id = req.params.id;
 
-    //Updates the speech_info table on the notes field
-    const queryText = `
-                        UPDATE speech_info 
-                        SET notes = $1
-                        WHERE id = $2;`;
-    pool.query(queryText,[notes,speech_id]).then((result) => {
-            res.sendStatus(204);
-        }).catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+        //Updates the speech_info table on the notes field
+        const queryText = `
+                            UPDATE speech_info 
+                            SET notes = $1
+                            WHERE id = $2;`;
+        pool.query(queryText,[notes,speech_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
  * PUT route uses UPDATE SQL
  * TABLE TOPICS
  */
-router.put('/table_topic/:id', (req, res) => {
-    let table_topics = req.body.table_topics;
-    let speech_id = req.params.id;
+router.put('/table_topic/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        let table_topics = req.body.table_topics;
+        let speech_id = req.params.id;
 
-    //Updates the speech_info table on the table_topics field
-    const queryText = `
-                        UPDATE speech_info 
-                        SET table_topics = $1
-                        WHERE id = $2;`;
-    pool.query(queryText,[table_topics,speech_id]).then((result) => {
-            res.sendStatus(204);
-        }).catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+        //Updates the speech_info table on the table_topics field
+        const queryText = `
+                            UPDATE speech_info 
+                            SET table_topics = $1
+                            WHERE id = $2;`;
+        pool.query(queryText,[table_topics,speech_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
  * PUT route uses UPDATE SQL
  * SPEECH TYPE
  */
-router.put('/speech_type/:id', (req, res) => {
-    let speech_type = req.body.speech_type;
-    let speech_id = req.params.id;
+router.put('/speech_type/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        let speech_type = req.body.speech_type;
+        let speech_id = req.params.id;
 
-    //Updates the speech_info table on the table_topics field
-    const queryText = `
-                        UPDATE speech_info 
-                        SET speech_type = $1
-                        WHERE id = $2;`;
-    pool.query(queryText,[speech_type,speech_id]).then((result) => {
-            res.sendStatus(204);
-        }).catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+        //Updates the speech_info table on the table_topics field
+        const queryText = `
+                            UPDATE speech_info 
+                            SET speech_type = $1
+                            WHERE id = $2;`;
+        pool.query(queryText,[speech_type,speech_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
  * PUT route uses UPDATE SQL
  * SPEECH EVAL
  */
-router.put('/eval/:id', (req, res) => {
-    let eval = req.body.speech_eval;
-    let speech_id = req.params.id;
+router.put('/eval/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        let eval = req.body.speech_eval;
+        let speech_id = req.params.id;
 
-    //Updates the speech_info table on the table_topics field
-    const queryText = `
-                        UPDATE speech_info 
-                        SET speech_eval = $1
-                        WHERE id = $2;`;
-    pool.query(queryText,[eval,speech_id]).then((result) => {
-            res.sendStatus(204);
-        }).catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+        //Updates the speech_info table on the table_topics field
+        const queryText = `
+                            UPDATE speech_info 
+                            SET speech_eval = $1
+                            WHERE id = $2;`;
+        pool.query(queryText,[eval,speech_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
  * PUT route uses UPDATE SQL
  * FINAL SPEECH UPDATES after end review is clicked
  */
-router.put('/review/:id', (req, res) => {
-    const speech_rt = req.body.speech_rt;
-    const speech_text = req.body.speech_text;
-    const and_count = req.body.and_count;
-    const like_count = req.body.like_count;
-    const in_time = req.body.in_time;
-    const status = req.body.status
-    const speech_id = req.params.id;
-    
+router.put('/review/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        const speech_rt = req.body.speech_rt;
+        const speech_text = req.body.speech_text;
+        const and_count = req.body.and_count;
+        const like_count = req.body.like_count;
+        const in_time = req.body.in_time;
+        const status = req.body.status
+        const speech_id = req.params.id;
+        
 
-    //Updates the speech_info table on the table_topics field
-    const queryText = `
-                        UPDATE speech_info 
-                        SET speech_rt = $1, speech_text = $2, and_count = $3,
-                        like_count = $4, in_time = $5, status = $6
-                        WHERE id = $7;`;
-    pool.query(queryText,[speech_rt, speech_text, and_count, like_count, in_time, status, speech_id]).then((result) => {
-            res.sendStatus(204);
-        }).catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+        //Updates the speech_info table on the table_topics field
+        const queryText = `
+                            UPDATE speech_info 
+                            SET speech_rt = $1, speech_text = $2, and_count = $3,
+                            like_count = $4, in_time = $5, status = $6
+                            WHERE id = $7;`;
+        pool.query(queryText,[speech_rt, speech_text, and_count, like_count, in_time, status, speech_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
